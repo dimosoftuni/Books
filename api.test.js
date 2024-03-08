ab7f6a9c92ec461d35565aa4ea4b418b7a230144
Dimo Mitev <dimosoftuni@gmail.com>
2024-03-08 11:27:08 +0200

    rework to ESM

--- api.test.js
+++ api.test.js
@@ -1,7 +1,7 @@
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('./server');
+import * as chai from 'chai';
+import chaiHttp from 'chai-http';
+import { server } from './server.js';
 const expect = chai.expect;
 
 chai.use(chaiHttp);
